6061ccfdaf2b6b0dd37f80e9bb5ad8fdd8b25173
Jenya <boss59842009@gmail.com>
2023-11-17 18:10:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from instructions import *
+
