b33ae940722104b453584b98e2b3e950042aac3d
Jenya <boss59842009@gmail.com>
2023-12-04 18:56:10 +0200

    add screens and timers

--- main.py
+++ main.py
@@ -6,17 +6,37 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from instructions import *
+from timer import Timer
+
+Window.clearcolor = (.33, .69, .76, 1)
+lbl_color = (1, .66, .34, 1)
+btn_color = (.29, .53, .94, 1)
+
+name = ""
+age = 0
+p1 = 0
+p2 = 0
+p3 = 0
+
+
+def check_int(str_num):
+    try:
+        return int(str_num)
+    except:
+        return False
 
 
 class InstrScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        instr = Label(text=txt_instruction, color=(1, .66, .34, 1), bold=True)
-        lbl_name = Label(text="Введіть імʼя:", halign='right', color=(1, .66, .34, 1), bold=True, font_size=40)
+        instr = Label(text=txt_instruction, color=lbl_color, bold=True)
+
+        lbl_name = Label(text="Введіть імʼя:", halign='right', color=lbl_color, bold=True, font_size=40)
         self.input_name = TextInput(text="Микола", multiline=False)
-        lbl_age = Label(text="Введіть вік:", halign='right', color=(1, .66, .34, 1), bold=True, font_size=40)
+        lbl_age = Label(text="Введіть вік:", halign='right', color=lbl_color, bold=True, font_size=40)
         self.input_age = TextInput(text="7", multiline=False)
-        self.btn = Button(text='Почати', size_hint=(.3, .2), pos_hint={'center_x': .5}, bold=True, background_color=(.29, .53, .94, 1))
+
+        self.btn = Button(text='Почати', size_hint=(.3, .2), pos_hint={'center_x': .5}, bold=True, background_color=btn_color)
         self.btn.on_press = self.next
 
         line1 = BoxLayout(size_hint=(.8, None), height="30sp")
@@ -37,19 +57,122 @@ class InstrScr(Screen):
         self.add_widget(main_line)
 
     def next(self):
-        self.manager.current = 'second'
+        global name, age
+        name = self.input_name.text
+        age = check_int(self.input_age.text)
+        if age <= 7 or age is False:
+            age = 7
+            self.input_age.text = str(age)
+        else:
+            self.manager.current = 'second'
+
 
 class PulseScr(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.next_screen = False
+
+        instr = Label(text=txt_test1, color=lbl_color, bold=True)
+
+        self.lbl_sec = Timer(15, color=lbl_color, bold=True)
+        lbl_result = Label(text="Введіть результат:", halign='right', color=lbl_color, bold=True, font_size=40)
+        self.input_result = TextInput(text="1", multiline=False)
+        self.input_result.set_disabled(True)
+        self.btn = Button(text='Почати', size_hint=(.3, .2), pos_hint={'center_x': .5}, bold=True,
+                          background_color=btn_color)
+        self.btn.on_press = self.next
+
+        main_line = BoxLayout(orientation='vertical', padding=10, spacing=15)
+        main_line.add_widget(instr)
+        main_line.add_widget(self.lbl_sec)
+        line = BoxLayout(size_hint=(.8, None), height="30sp")
+        line.add_widget(lbl_result)
+        line.add_widget(self.input_result)
+        main_line.add_widget(line)
+        main_line.add_widget(self.btn)
+
+        self.add_widget(main_line)
+
+    def next(self):
+        global p1
+        if not self.next_screen:
+            self.input_result.set_disabled(False)
+            self.lbl_sec.start()
+        else:
+            p1 = check_int(self.input_result.text)
+            if p1 is False or p1 <= 1:
+                p1 = 0
+                self.input_result.text = str(p1)
+            else:
+                self.manager.current = 'third'
+
 
 class SitsScr(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_sits, color=lbl_color, bold=True)
+
+        self.sits = Label(text='Залишилось присідань: 30', color=lbl_color, bold=True)
+        self.btn = Button(text='Почати', size_hint=(.3, .2), pos_hint={'center_x': .5}, bold=True,
+                          background_color=btn_color)
+        self.btn.on_press = self.next
+
+        main_line = BoxLayout(orientation='vertical', padding=10, spacing=15)
+        main_line.add_widget(instr)
+        main_line.add_widget(self.sits)
+        main_line.add_widget(self.btn)
+
+        self.add_widget(main_line)
+
+    def next(self):
+        self.manager.current = 'fourth'
+
 
 class PulseScr2(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_test3, color=lbl_color, bold=True)
+        lbl_pulse = Label(text="Рахуйте пульс", color=lbl_color, bold=True)
+        self.lbl_sec = Label(text="Пройшло секунд: 0", color=lbl_color, bold=True)
 
+        lbl_result = Label(text="Результат:", halign='right', color=lbl_color, bold=True, font_size=40)
+        self.input_result = TextInput(text="0", multiline=False)
+        lbl_after_res = Label(text="Результат після відпочинку:", halign='right', color=lbl_color, bold=True, font_size=40)
+        self.input_after_res = TextInput(text="0", multiline=False)
+
+        self.btn = Button(text='Почати', size_hint=(.3, .4), pos_hint={'center_x': .5}, bold=True,
+                          background_color=btn_color)
+        self.btn.on_press = self.next
+
+        main_line = BoxLayout(orientation='vertical', padding=10, spacing=15)
+        main_line.add_widget(instr)
+        main_line.add_widget(lbl_pulse)
+        main_line.add_widget(self.lbl_sec)
+
+        line1 = BoxLayout(size_hint=(.8, None), height="30sp")
+        line2 = BoxLayout(size_hint=(.8, None), height="30sp")
+        line1.add_widget(lbl_result)
+        line1.add_widget(self.input_result)
+        line2.add_widget(lbl_after_res)
+        line2.add_widget(self.input_after_res)
+
+        main_line.add_widget(line1)
+        main_line.add_widget(line2)
+        main_line.add_widget(self.btn)
+
+        self.add_widget(main_line)
+
+    def next(self):
+        self.manager.current = 'fifth'
 class ResultScr(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.instr = Label(text="Ваш індекс Руфʼє:-14.8")
+        self.index = Label(text="Працездатність серця: висока")
+        main_line = BoxLayout(orientation="vertical", size_hint=(.5, .1), pos_hint={'center_x': .5, 'center_y': .5})
+        main_line.add_widget(self.instr)
+        main_line.add_widget(self.index)
+        self.add_widget(main_line)
 
 
 class HeartCheck(App):
